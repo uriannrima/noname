7f8f177de537ab394e972f54fe050314c8361dd0
luciano-lima <ext.luciano.lima@farfetch.com>
2020-10-16 18:57:03 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script lang="tsx">
+import { IonApp, IonRouterOutlet } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    return () => (
+      <IonApp>
+      <div>Hi1</div>
+      </IonApp>
+    );
+  },
+});
+</script>
